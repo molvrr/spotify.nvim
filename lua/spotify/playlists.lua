897dfe9ab5c994d9a472288b429cfb36bb4c9af0
Mateus Cruz <mateuscolvr@gmail.com>
2023-05-06 21:12:39 -0300

    adiciona busca de playlists

---
+++ lua/spotify/playlists.lua
@@ -0,0 +1,22 @@
+local M = {}
+
+local Curl = require('plenary.curl')
+
+function M.get_playlists()
+  local resp = Curl.get('https://api.spotify.com/v1/me/playlists', {
+    query = {
+      limit = 50
+    },
+    headers = {
+      authorization = 'Bearer ' .. vim.g['spotify-token']
+    }
+  })
+
+  if resp.status == 200 then
+    return vim.json.decode(resp.body).items
+  else
+    return {}
+  end
+end
+
+return M
